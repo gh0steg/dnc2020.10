e2e8d9bbbe611c47c796812fd9f40c88c4ebcc56
Dan Efremov <efremov.d@gmail.com>
2020-11-09 01:12:35 +0500

    vertical flip added

--- ConvolutionWpf/ConvolutionWpf/Commands/FlipCommand.cs
+++ ConvolutionWpf/ConvolutionWpf/Commands/FlipCommand.cs
@@ -29,13 +29,13 @@ namespace ConvolutionWpf.Commands
             //public WriteableBitmap(int pixelWidth,int pixelHeight,double dpiX,double dpiY,PixelFormat pixelFormat,BitmapPalette palette)
             //инициализация нового WriteableBitmap
 
-            var newImage = new WriteableBitmap(image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
+            var newImage = new WriteableBitmap(image.PixelWidth*2, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
             //инициализация newImage с двойным размером по ширине image
             var pixels = new byte[image.PixelHeight * image.BackBufferStride];
             //инициализация массива pixels для считывания image
             image.CopyPixels(pixels, image.BackBufferStride, 0);
             //копируем массив точек в pixels
-            var newPixels = new byte[image.PixelHeight * image.BackBufferStride];
+            var newPixels = new byte[image.PixelHeight * image.BackBufferStride*2];
             var pixelsNormal = new byte[image.BackBufferStride];
             var pixelsReverse = new byte[image.BackBufferStride];
             for (int i = 0; i < image.PixelHeight; i++)
@@ -44,16 +44,16 @@ namespace ConvolutionWpf.Commands
                 for (int j = 0; j < image.PixelWidth; j++)
                 {
                     int index = i * image.BackBufferStride + 4 * j;
-                    for (int c = 0; c < 4; ++c)
+                    for (int c = 0; c < 4; ++c) //поворачиваем по 4 байта, чтобы сохранить последовательность RGBA
                     {
-                        pixelsReverse[image.BackBufferStride - 4 * (j+1) + c] = pixels[index + c];
+                        pixelsReverse[image.BackBufferStride - 4 * (j+1) + c] = pixels[index + c];//пишем массим реверсивной строки
+                        pixelsNormal[4 * j + c] = pixels[index + c]; //пишем массив нормальной строки
                     }
                 }
-                pixelsReverse.CopyTo(newPixels,i*image.PixelHeight);
+                    pixelsNormal.CopyTo(newPixels, i * newImage.BackBufferStride)//копируем массив в результирующий;
+                    pixelsReverse.CopyTo(newPixels, i * newImage.BackBufferStride + image.BackBufferStride)//добавляем сдвиг;
             }
-            newImage.WritePixels(new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight), newPixels, image.BackBufferStride, 0);
-            
-
+            newImage.WritePixels(new Int32Rect(0, 0, newImage.PixelWidth, newImage.PixelHeight), newPixels, newImage.BackBufferStride, 0);
 
             OnImageChanged?.Invoke(newImage); //отправляем на отрисовку картинку
         }
